97f0d58b8a330f36a94bca5c6f1a20dc0af99096
Valeria Mora <valemora2018@gmail.com>
2023-11-06 14:53:47 -0600

    Agregando programa para corregir

--- 01-vite-vanilla/src/components/HellOwOrld.js
+++ 01-vite-vanilla/src/components/HellOwOrld.js
@@ -1,3 +1,6 @@
-const HolaMundo = () => {
-    <h1>Hola Mundo Vite JS Vanilla</h1>
-}
+const HolaMundo = () =>
+    `<h1>Hola Mundo Vite JS Vanilla</h1>
+    <p>Contenido desde una template string</p>
+    `
+
+export default HolaMundo
